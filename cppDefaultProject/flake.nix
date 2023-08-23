37eb3629cb32fb95849a4177e4675d2438f7dc56
semssk <ssksem015@gmail.com>
2023-08-23 18:46:45 +0100

    Added c++ default project template

---
+++ cppDefaultProject/flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {nixpkgs,flake-utils,...}:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells = {
+        default = pkgs.mkShell {
+          name = "cpp Shell";
+          buildInputs = with pkgs;[
+            pkg-config
+            cmake
+            cmake-language-server
+            ninja
+            clang-tools
+            gcc
+          ];
+        };
+      };
+    });
+}
